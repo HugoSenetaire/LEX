0f6aa3b4cd58670f711b25e666704733babb0395
Hugo Henri Joseph Sénétaire <hhjs@dtu.dk>
2022-01-04 22:13:57 +0100

    test

--- missingDataTrainingModule/Classification/post_process_imputation.py
+++ missingDataTrainingModule/Classification/post_process_imputation.py
@@ -7,6 +7,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from utils_missing import *
 import numpy as np
 import torch
+import torch.nn as nn
 import copy
 from importlib import import_module
 import matplotlib.pyplot as plt
