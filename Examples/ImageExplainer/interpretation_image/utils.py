d84f37f1d8fae8b73bab110a3fd6be848b15ad00
Hugo Henri Joseph Sénétaire <hhjs@dtu.dk>
2022-01-13 23:46:18 +0100

    test

--- Examples/ImageExplainer/interpretation_image/utils.py
+++ Examples/ImageExplainer/interpretation_image/utils.py
@@ -120,7 +120,7 @@ def image_f1_score(trainer, loader, final_path, nb_samples_image = 20):
     data, target, index= next(iter(loader.test_loader))
     data = data[:nb_samples_image]
     target = target[:nb_samples_image]
-    # quadrant = loader.dataset.quadrant_test[index]
+    quadrant = loader.dataset.quadrant_test[index]
     wanted_shape = data[0].shape
 
     
@@ -149,15 +149,15 @@ def image_f1_score(trainer, loader, final_path, nb_samples_image = 20):
             os.makedirs(target_path)
         x_original = data[k].reshape(wanted_shape)
         # if 
-        # current_quadrant = quadrant[k].reshape(wanted_shape)
+        current_quadrant = quadrant[k].reshape(wanted_shape)
         current_pi_list = argmax_pi_list[k].reshape(wanted_shape) 
-        fig, axs = plt.subplots(1,2, figsize=(10,5))
+        fig, axs = plt.subplots(1,3, figsize=(15,5))
         axs[0].imshow(x_original, cmap='gray', interpolation='none',)
         axs[1].imshow(current_pi_list, cmap='gray', interpolation='none', vmin = 0., vmax = 1.0)
-        # axs[2].imshow(current_quadrant, cmap='gray', interpolation='none', vmin = 0., vmax = 1.0)
+        axs[2].imshow(current_quadrant, cmap='gray', interpolation='none', vmin = 0., vmax = 1.0)
         
-        # f1_score = metrics.f1_score(current_quadrant.flatten(), current_pi_list.flatten(),)
-        # plt.title(f"F1_score {f1_score:.5f}")
+        f1_score = metrics.f1_score(current_quadrant.flatten(), current_pi_list.flatten(),)
+        plt.title(f"F1_score {f1_score:.5f}")
         plt.savefig(os.path.join(target_path, f"{k}.png"))
         plt.close(fig)
     
@@ -174,7 +174,7 @@ def accuracy_output(trainer, loader, final_path, batch_size = 100):
     for aux in iter(loader.test_loader):
         data, target, index=aux
         wanted_shape = data[0].shape
-        # quadrant_test = loader.dataset.quadrant_test[index].detach().numpy()
+        quadrant_test = loader.dataset.quadrant_test[index].detach().numpy()
         if trainer.use_cuda:
             data, target, = data.cuda(), target.cuda()
 
@@ -185,21 +185,21 @@ def accuracy_output(trainer, loader, final_path, batch_size = 100):
     
         data = data.cpu().detach().numpy()
         pi_list = pi_list.cpu().detach().numpy()
-        # accuracy = np.sum( np.abs(quadrant_test - pi_list))
+        accuracy = np.sum( np.abs(quadrant_test - pi_list))
 
         argmax_pi_list = np.round(pi_list).astype(int)
         argmax_pi_list = argmax_pi_list.reshape(batch_size, -1)
-        # quadrant_test = quadrant_test.reshape(batch_size, -1)
-        # for pi,current_quadrant in zip(argmax_pi_list, quadrant_test):
-            # f1_score_avg += metrics.f1_score(current_quadrant, pi,)
+        quadrant_test = quadrant_test.reshape(batch_size, -1)
+        for pi,current_quadrant in zip(argmax_pi_list, quadrant_test):
+            f1_score_avg += metrics.f1_score(current_quadrant, pi,)
         target = target.cpu().detach().numpy()
         complete_size+=batch_size
 
-    # mean_accuracy = accuracy/complete_size/np.prod(wanted_shape)
-    # f1_score_avg = f1_score_avg/complete_size
-    # with open(os.path.join(final_path, "accuracy_output.txt"), "w") as f:
-        # f.write(f"mean accuracy: {mean_accuracy}\n")
-        # f.write(f"f1 score: {f1_score_avg}")
+    mean_accuracy = accuracy/complete_size/np.prod(wanted_shape)
+    f1_score_avg = f1_score_avg/complete_size
+    with open(os.path.join(final_path, "accuracy_output.txt"), "w") as f:
+        f.write(f"mean accuracy: {mean_accuracy}\n")
+        f.write(f"f1 score: {f1_score_avg}")
         
        
     
