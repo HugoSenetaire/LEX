67fa4798eb94a4fe53e05dfc0a6c8bd198952eb2
Hugo Henri Joseph Sénétaire <hhjs@dtu.dk>
2022-01-16 11:11:48 +0100

    debug

--- missingDataTrainingModule/main_launcher.py
+++ missingDataTrainingModule/main_launcher.py
@@ -269,6 +269,7 @@ def experiment(args_output, args_dataset, args_classification, args_selection, a
 
         vanilla_classification_module = ClassificationModule(classifier, imputation = imputation)
         if args_train["use_cuda"]:
+            print("Here use cuda")
             vanilla_classification_module.cuda()
         optim_classifier = args_compiler["optim_classification"](vanilla_classification_module.parameters())
 
