Fix: Change the calculation for neg likelihood in test, Feat : Can fix the selector parameters, can use regularization while pretraining selector

Examples/ArtificialRegression/default_parameter.py
@@ -126,6 +126,7 @@ def get_default():
     args_train["nb_epoch"] = 10 # Training the complete model
     args_train["nb_epoch_post_hoc"] = 0 # Training post_hoc
     args_train["nb_epoch_pretrain_selector"] = 0 # Pretrain selector
+    args_train["use_regularization_pretrain_selector"] = False # Use regularization when pretraining the selector
     args_train["nb_epoch_pretrain"] = 0 # Training the complete model 
     args_train["nb_sample_z_train_monte_carlo"] = 1
     args_train["nb_sample_z_train_IWAE"] = 1  # Number K in the IWAE-similar loss
@@ -157,6 +158,6 @@ def get_default():
     args_compiler["scheduler_post_hoc"] = partial(torch.optim.lr_scheduler.StepLR, step_size=1000, gamma = 0.9)
     
     args_test = {}
-    args_test["nb_sample_z_test"] = 1
+    args_test["nb_sample_z_test"] = 10
 
     return  args_output, args_dataset, args_classification, args_selection, args_distribution_module, args_complete_trainer, args_train, args_test, args_compiler

missingDataTrainingModule/interpretation_training.py
@@ -19,6 +19,7 @@ class SELECTION_BASED_CLASSIFICATION():
                 baseline = None,
                 reshape_mask_function = None,
                 fix_classifier_parameters = False,
+                fix_selector_parameters = False,
                 post_hoc = False,
                 post_hoc_guidance = None,
                 argmax_post_hoc = False,
@@ -45,6 +46,7 @@ class SELECTION_BASED_CLASSIFICATION():
 
       
         self.fix_classifier_parameters = fix_classifier_parameters
+        self.fix_selector_parameters = fix_selector_parameters
         self.post_hoc_guidance = post_hoc_guidance
         self.post_hoc = post_hoc
         self.argmax_post_hoc = argmax_post_hoc
@@ -74,15 +76,17 @@ class SELECTION_BASED_CLASSIFICATION():
 
     def compile(self, optim_classification, optim_selection, scheduler_classification = None, scheduler_selection = None, optim_baseline = None, scheduler_baseline = None, optim_distribution_module = None, scheduler_distribution_module = None, **kwargs):
         self.optim_classification = optim_classification
+        if self.optim_classification is None :
+            self.fix_classifier_parameters = True
         self.scheduler_classification = scheduler_classification
         self.optim_selection = optim_selection
+        if self.optim_selection is None :
+            self.fix_selector_parameters = True
         self.scheduler_selection = scheduler_selection
         self.optim_distribution_module = optim_distribution_module
         self.scheduler_distribution_module = scheduler_distribution_module
 
         self.compiled = True
-
-
 
     def zero_grad(self):
         self.classification_module.zero_grad()
@@ -122,12 +126,14 @@ class SELECTION_BASED_CLASSIFICATION():
         assert(self.compiled)
         if (not self.fix_classifier_parameters) and (self.optim_classification is not None) :
            self.optim_classification.step()
-        self.optim_selection.step()
+        if (not self.fix_selector_parameters) and (self.optim_selection is not None) :
+            self.optim_selection.step()
         if self.optim_distribution_module is not None :
             self.optim_distribution_module.step()
-
-    
+   
     def train_epoch(self, epoch, loader, nb_sample_z_monte_carlo = 3, nb_sample_z_IWAE = 3, save_dic=False, verbose=True):
+        if self.fix_classifier_parameters and self.fix_selector_parameters :
+            raise AttributeError("You can't train if both classifiers and selectors are fixed.")
         assert(self.compiled)
         self.train()
         total_dic = {}
@@ -212,8 +218,6 @@ class SELECTION_BASED_CLASSIFICATION():
         return neg_likelihood
 
 
-
-
     def _create_dic(self, loss_total, neg_likelihood, mse_loss, pi_list, loss_rec = None, loss_reg = None, loss_selection = None, ):
         # dic = super()._create_dic(loss_total, neg_likelihood, mse_loss)
         dic = {}
@@ -238,7 +242,6 @@ class SELECTION_BASED_CLASSIFICATION():
         return dic
 
     
-
     def _train_step(self, data, target, dataset, index = None, nb_sample_z_monte_carlo = 3, nb_sample_z_IWAE = 3, need_dic = False):
         self.zero_grad()
 
@@ -267,7 +270,7 @@ class SELECTION_BASED_CLASSIFICATION():
                         )
 
         loss_s, loss_f = self.monte_carlo_gradient_estimator(cost_calculation, pi_list, nb_sample_z_monte_carlo)
-
+        
         if self.monte_carlo_gradient_estimator.combined_grad_f_s :
             loss_total = loss_reg + loss_s # How to treat differently for REINFORCE or REPARAM ?
         else :
@@ -329,14 +332,11 @@ class SELECTION_BASED_CLASSIFICATION():
         neg_likelihood = torch.logsumexp(neg_likelihood, axis=-1) - torch.log(torch.tensor(nb_sample_z_IWAE).type(torch.float32))
 
         return neg_likelihood
-
-
-        
-    
+  
 
     def test(self, loader, nb_sample_z = 10):
-        test_loss_mse = 0
-        test_loss_likelihood = 0
+        mse_loss = 0
+        neg_likelihood = 0
         correct_classic = 0
         correct_post_hoc = 0
         correct_destructed = 0
@@ -361,7 +361,7 @@ class SELECTION_BASED_CLASSIFICATION():
                 log_pi_list, _ = self.selection_module(data)
                 pi_list_total.append(torch.exp(log_pi_list).cpu().numpy())
                 self.distribution_module(torch.exp(log_pi_list))
-                z= self.distribution_module.sample((nb_sample_z,))
+                z = self.distribution_module.sample((nb_sample_z,))
                 z = self.reshape(z)
                 
 
@@ -380,21 +380,21 @@ class SELECTION_BASED_CLASSIFICATION():
                 log_y_hat_destructed, _ = self.classification_module(data_expanded.flatten(0,1), z, index = index_expanded_flatten)
                 
                 log_y_hat_destructed = log_y_hat_destructed.reshape(nb_sample_z, batch_size, loader.dataset.get_dim_output())
-                log_y_hat_iwae = torch.logsumexp(log_y_hat_destructed,0) - torch.log(torch.tensor(nb_sample_z).type(torch.float32))
                 log_y_hat_mean = torch.mean(log_y_hat_destructed, axis=0)
                 index = torch.where(torch.any(torch.isnan(log_y_hat_destructed), axis=-1))[1]
 
-                test_loss_likelihood += F.nll_loss(log_y_hat_destructed.flatten(0,1),target_expanded.flatten(0,1))
-                test_loss_mse += torch.mean(torch.sum((torch.exp(log_y_hat_mean)-one_hot_target)**2,1))
+                neg_likelihood += F.nll_loss(log_y_hat_destructed.flatten(0,1),target_expanded.flatten(0,1), reduce=False).reshape((nb_sample_z, batch_size)).mean(0).sum()
+                mse_loss += torch.sum(torch.sum((torch.exp(log_y_hat_mean)-one_hot_target)**2,1))
 
                 pred_destructed = torch.argmax(log_y_hat_mean, dim=1)
                 correct_destructed += pred_destructed.eq(target).sum()
 
 
 
-            test_loss_mse /= len(loader.test_loader.dataset) * batch_size
+            mse_loss /= len(loader.test_loader.dataset) 
+            neg_likelihood /= len(loader.test_loader.dataset) 
             print('\nTest set: MSE: {:.4f}, Likelihood {:.4f}, Accuracy No selection: {}/{} ({:.0f}%), Accuracy selection: {}/{} ({:.0f}%), Accuracy PostHoc: {}/{} ({:.0f}%),'.format(
-                -test_loss_likelihood.item(), test_loss_mse.item(),
+                 mse_loss.item(), -neg_likelihood.item(),
                  correct_classic.item(), len(loader.test_loader.dataset),100. * correct_classic.item() / len(loader.test_loader.dataset),
                  correct_destructed.item(), len(loader.test_loader.dataset), 100. * correct_destructed.item() / len(loader.test_loader.dataset),
                  correct_post_hoc, len(loader.test_loader.dataset), 100. * correct_post_hoc / len(loader.test_loader.dataset),
@@ -402,8 +402,8 @@ class SELECTION_BASED_CLASSIFICATION():
             print("\n")
             total_dic = self._create_dic_test(correct_destructed/len(loader.test_loader.dataset),
                 correct_classic/len(loader.test_loader.dataset),
-                test_loss_likelihood,
-                test_loss_mse,
+                neg_likelihood,
+                mse_loss,
                 pi_list_total,
                 correct_post_hoc=correct_post_hoc)
 
@@ -505,24 +505,24 @@ class REALX(SELECTION_BASED_CLASSIFICATION):
             self.zero_grad()
 
 
+        if not self.fix_selector_parameters :
+            cost_calculation = partial(self.calculate_cost,
+                            data_expanded_multiple_imputation = data_expanded_multiple_imputation,
+                            target_expanded_multiple_imputation = target_expanded_multiple_imputation,
+                            index_expanded_multiple_imputation = index_expanded_multiple_imputation,
+                            one_hot_target_expanded_multiple_imputation = one_hot_target_expanded_multiple_imputation,
+                            dim_output = dataset.get_dim_output(),
+                            )
 
-        cost_calculation = partial(self.calculate_cost,
-                        data_expanded_multiple_imputation = data_expanded_multiple_imputation,
-                        target_expanded_multiple_imputation = target_expanded_multiple_imputation,
-                        index_expanded_multiple_imputation = index_expanded_multiple_imputation,
-                        one_hot_target_expanded_multiple_imputation = one_hot_target_expanded_multiple_imputation,
-                        dim_output = dataset.get_dim_output(),
-                        )
-
-        loss_s, _ = self.monte_carlo_gradient_estimator(cost_calculation, pi_list, nb_sample_z_monte_carlo)
+            loss_s, _ = self.monte_carlo_gradient_estimator(cost_calculation, pi_list, nb_sample_z_monte_carlo)
 
 
-        loss_total = loss_reg + loss_s # How to treat differently for REINFORCE or REPARAM ?
+            loss_total = loss_reg + loss_s # How to treat differently for REINFORCE or REPARAM ?
 
-        torch.mean(loss_total).backward()
-        self.optim_selection.step()
-        if self.optim_distribution_module is not None :
-            self.optim_distribution_module.step()
+            torch.mean(loss_total).backward()
+            self.optim_selection.step()
+            if self.optim_distribution_module is not None :
+                self.optim_distribution_module.step()
 
         if need_dic :
             if self.fix_classifier_parameters :

missingDataTrainingModule/selection_training.py
@@ -1,10 +1,13 @@
+from missingDataTrainingModule.Classification import classification_module
 from .utils_missing import *
 import torch.nn.functional as F
 
 
 class selectionTraining():
-    def __init__(self, selection_module,):    
+    def __init__(self, selection_module, use_reg = False):    
         self.selection_module = selection_module
+        self.use_reg = use_reg
+
         self.compiled = False
         self.use_cuda = False
 
@@ -22,9 +25,13 @@ class selectionTraining():
             self.use_cuda = True
        
 
-    def _create_dic(self, mse_loss):
+    def _create_dic(self, mse_loss, loss_reg = None):
         dic = {}
         dic["mse_loss"] = mse_loss.item()
+        if loss_reg is not None :
+            dic["loss_reg"] = loss_reg.item()
+        else :
+            dic["loss_reg"] = 0
         return dic
 
     def _create_dic_test(self, correct, mse_loss):
@@ -44,11 +51,18 @@ class selectionTraining():
         self.zero_grad()
         if self.use_cuda :
             data, target, index = on_cuda(data, target, index)
-        log_pi_list, _ = self.selection_module(data,)
+        
+        log_pi_list, loss_reg = self.selection_module(data,)
 
-        mse_loss = F.mse_loss(torch.exp(log_pi_list), target)
-        dic = self._create_dic(mse_loss, )
-        mse_loss.backward()
+        mse_loss = F.mse_loss(torch.exp(log_pi_list), target, reduction='mean')
+
+        if self.use_reg :
+            dic = self._create_dic(mse_loss, loss_reg)
+            (mse_loss + loss_reg).backward()
+        else :
+            dic = self._create_dic(mse_loss, )
+            mse_loss.backward()
+
         self.optim_selection.step()
         return dic
 