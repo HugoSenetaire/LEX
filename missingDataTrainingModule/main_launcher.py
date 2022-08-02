caae8e84d3b9c9c52157315085501ae5522d16b1
HugoSenetaire <hhjs@dtu.dk>
2022-08-02 17:12:34 +0200

    fix temeprature

--- missingDataTrainingModule/main_launcher.py
+++ missingDataTrainingModule/main_launcher.py
@@ -1,10 +1,6 @@
 import os
 import torch
 import torch.nn as nn
-import inspect
-
-
-from missingDataTrainingModule.utils.loss import NLLLossAugmented
 
 from .classification_training import ordinaryTraining, EVAL_X, trueSelectionTraining
 from .interpretation_training import SELECTION_BASED_CLASSIFICATION, REALX
