a53e5f2d9b27d0571688b611a603aa09c16bde65
HugoSenetaire <hhjs@dtu.dk>
2022-08-11 13:07:43 +0200

    handle deletion of cuda memory

--- Examples/ArtificialRegression/multiple_experiment_launcher.py
+++ Examples/ArtificialRegression/multiple_experiment_launcher.py
@@ -77,6 +77,12 @@ def multiple_experiment(
             plt.scatter(loader.dataset.X[:10000,0], loader.dataset.X[:10000,1], c =Y, alpha = 0.15, cmap = 'gray', vmin = np.min(Y), vmax = np.max(Y))
             plt.savefig(out_path)
             plt.close()
+        del trainer, loader, dic_list
+        try :
+            torch.cuda.empty_cache()
+        except BaseException as e:
+            print(e)
+            print("Can't empty the cache")
         return count
     except Exception as e :
         print(traceback.format_exc())
@@ -88,5 +94,12 @@ def multiple_experiment(
         with open(complete_args.args_output.path+"_error/error.txt", "w") as f:
             f.write(str(e))
             f.write(traceback.format_exc())
+        
+        del trainer, loader, dic_list
+        try :
+            torch.cuda.empty_cache()
+        except BaseException as e:
+            print(e)
+            print("Can't empty the cache")
         return count
 
