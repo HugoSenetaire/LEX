4da43b31f9abb257f02f062045c4094239e7c67d
HugoSenetaire <hhjs@dtu.dk>
2022-08-31 14:49:02 +0200

    Feat: add encode and decode to UNET autoencoder

--- missingDataTrainingModule/Prediction/autoencoder_wrapper.py
+++ missingDataTrainingModule/Prediction/autoencoder_wrapper.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 from ..Selection.selective_network import *
 
 class AutoEncoderWrapper(nn.Module):
-    def __init__(self, input_size = (1,28,28), output_size= (1, 28, 28), kernel_size = (1,1), kernel_stride = (1,1), bilinear = True, log2_min_channel = 6):
+    def __init__(self, input_size = (1,28,28), output_size= (1, 28, 28), kernel_size = (1,1), kernel_stride = (1,1), bilinear = True, log2_min_channel = 2):
         super(AutoEncoderWrapper, self).__init__()
         self.autoencoder = SelectorUNET(input_size = input_size, output_size=output_size, bilinear = bilinear, kernel_size= kernel_size, kernel_stride = kernel_stride, log2_min_channel = log2_min_channel)
         self.input_size = input_size
@@ -11,6 +11,10 @@ class AutoEncoderWrapper(nn.Module):
         self.kernel_stride = kernel_stride
         self.bilinear = bilinear
         self.log2_min_channel = log2_min_channel
-        
+
     def __call__(self, x):
         return self.autoencoder(x)
+    def encode(self, x):
+        return self.autoencoder.encode(x)
+    def decode(self, x):
+        return self.autoencoder.decode(x)
