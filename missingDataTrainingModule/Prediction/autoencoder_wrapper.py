97b2ffcf8ce71e1dc74f71512206d03a0c65b62b
HugoSenetaire <hhjs@dtu.dk>
2022-09-06 17:31:44 +0200

    Feat: change default autoencoder latent size

--- missingDataTrainingModule/Prediction/autoencoder_wrapper.py
+++ missingDataTrainingModule/Prediction/autoencoder_wrapper.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 from ..Selection.selective_network import *
 
 class AutoEncoderWrapper(nn.Module):
-    def __init__(self, input_size = (1,28,28), output_size= (1, 28, 28), kernel_size = (1,1), kernel_stride = (1,1), bilinear = True, log2_min_channel = 2):
+    def __init__(self, input_size = (1,28,28), output_size= (1, 28, 28), kernel_size = (1,1), kernel_stride = (1,1), bilinear = True, log2_min_channel = 1):
         super(AutoEncoderWrapper, self).__init__()
         self.autoencoder = SelectorUNET(input_size = input_size, output_size=output_size, bilinear = bilinear, kernel_size= kernel_size, kernel_stride = kernel_stride, log2_min_channel = log2_min_channel)
         self.input_size = input_size
