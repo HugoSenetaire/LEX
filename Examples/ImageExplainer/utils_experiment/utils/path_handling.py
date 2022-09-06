75a52b439871d0abb60e2afc5f940808c90a7e2c
HugoSenetaire <hhjs@dtu.dk>
2022-09-06 17:27:57 +0200

    Fix: change create name to get more readable folder

--- Examples/ImageExplainer/utils_experiment/utils/path_handling.py
+++ Examples/ImageExplainer/utils_experiment/utils/path_handling.py
@@ -5,7 +5,11 @@ def create_name(args, dataset_name, name_experiment, count):
     current_name_experiment = os.path.join(name_experiment, dataset_name)
     origin_path = args.args_output.path
     path_global = os.path.join(origin_path, current_name_experiment)
-    aux_string = f"{args.args_trainer.complete_trainer}_{args.args_trainer.monte_carlo_gradient_estimator}_{args.args_selection.regularization}_{args.args_selection.loss_regularization}_{args.args_selection.batched}_{args.args_classification.imputation}"
+    aux_string = f"{args.args_trainer.complete_trainer}_{args.args_trainer.monte_carlo_gradient_estimator}_{args.args_classification.imputation}"
+    if args.args_classification.imputation == "ModuleImputation":
+        aux_string += f"_{args.args_classification.module_imputation}"
+    if args.args_classification.imputation == "ConstantImputation":
+        aux_string += f"_{args.args_classification.cste_imputation}"
     current_path = os.path.join(path_global, aux_string)
     current_path = os.path.join(current_path, f"{args.args_classification.classifier}_{args.args_selection.selector}_{args.args_selection.rate}_{count}")
     args.args_output.path = current_path
